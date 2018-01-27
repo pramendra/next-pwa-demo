3bd8eae28b04c50a2007a3a831ccc56e33084325
Pramendra Gupta <pramendra@gmail.com>
2018-01-26 16:33:49 -0800

    added support for preact

--- next.config.js
+++ next.config.js
@@ -3,5 +3,15 @@ module.exports = {
     '/': { page: '/' },
     '/category': { page: '/category' },
     '/pdp': { page: '/pdp' }
-  })
+  }),
+  webpack: (config, { dev }) => {
+    if (dev) {
+      return config;
+    }
+    config.resolve.alias = {
+      react: 'preact-compat/dist/preact-compat',
+      'react-dom': 'preact-compat/dist/preact-compat'
+    };
+    return config;
+  }
 };
