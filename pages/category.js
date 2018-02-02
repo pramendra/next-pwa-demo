e4949d32ac323249af509e9cdfa29ea366f1e9a8
Pramendra Gupta <pramendra@gmail.com>
2018-02-01 22:22:09 -0800

    delete unused file

--- pages/category.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-export default () => (
-  <div>
-    Welcome to category page!
-    <Link prefetch href="/pdp">
-      <a>Product detail page</a>
-    </Link>
-  </div>
-);
