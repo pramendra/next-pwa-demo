e5f8ab06ba4ebdd98a05328e6e9928edafbda3f4
Pramendra Gupta <pramendra@gmail.com>
2018-02-01 22:22:43 -0800

    added fetch

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -1,3 +1,8 @@
 // @flow
 
 export const renderNull = () => null;
+
+export async function fetchAPI(url: string) {
+  const data = await window.fetch(url).then(res => res.json().then(d => d));
+  return data;
+}
